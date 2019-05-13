ea9afbdb325b6ef692e6bb91d0399d3c224c1fbe
Clive Darra <cd@example.com>
2019-05-13 16:03:07 +0100

    ver 2

---
+++ about/index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const helmet = require('helmet')
+
+const app = express()
+
+// add some security-related headers to the response
+app.use(helmet())
+
+app.get('*', (req, res) => {
+    res.set('Content-Type', 'text/html')
+    res.send(200, `
+        <h1>Hello from ZEIT NOW path '/about' on Now 2.0!</h1>
+        <h2>Go to <a href="/">/</a></h2>
+    `)
+})
+
+module.exports = app
---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const helmet = require('helmet')
+
+const app = express()
+
+// add some security-related headers to the response
+app.use(helmet())
+
+app.get('*', (req, res) => {
+    res.set('Content-Type', 'text/html')
+    res.status(200).send(`
+        <h1>Hello from ZEIT NOW path '/' on Now 2.0!</h1>
+        <h2>Go to <a href="/about">/about</a></h2>
+    `)
+})
+
+module.exports = app
