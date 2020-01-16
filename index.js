74792ac64ded0b2e2362103c2a286f24c8708c5c
Clive Da <cliveda@example.com>
2020-01-16 00:46:09 +0000

    cc

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
+const GLITCH=process.env.GLITCH
+const ZEIT=process.env.ZEIT
+
+const APINAME=process.env.CLOUDINARY_APINAME
+const APIKEY=process.env.CLOUDINARY_APIKEY
+const APISECRET=process.env.CLOUDINARY_APISECRET
+
 const express = require('express')
+const cloudy = require('cloudinary').v2
 
 const app = express()
 
-app.get('*', (req, res) => {
+cloudy.config({ 
+  cloud_name: APINAME, 
+  api_key: APIKEY, 
+  api_secret: APISECRET
+});
+
+cloudy.api.resources(
+  { type: 'upload', max_results: 4 }, 
+  function(error, result) {console.log(result, error); });
+
+app.get('*', async (req, res) => {
     res.set('Content-Type', 'text/html')
     res.status(200).send(`
         <h1>ZEIT NODE EXPRESS</h1>
@@ -10,4 +28,11 @@ app.get('*', (req, res) => {
     `)
 })
 
-module.exports = app
+if (GLITCH) {
+  app.listen(process.env.PORT)
+}
+
+// if (ZEIT)
+  module.exports = app
+//
+
